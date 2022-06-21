d411a6a24fe7dcd664c08849a694fb71790ad0f5
Ace <kronicrespawn@gmail.com>
2022-06-21 11:21:04 -0500

    added custom board, 3 board sizes, README.md

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Etch
 Etch-A-Sketch Project
+
+How it works: Players will start the game by first picking a board size. The cells within the board will change color to red the mouse is the mouse is hovered over it. Be creative and draw to your hearts content. Moving the mouse slow allows for more accurate drawing while moving quickly in short burst allows you to essentially lift your drawing marker to a different spot without turning a cell red.
+ 
+To erase the board, pick any board size (even the same one) and the board will be erased, allowing players to start drawing on a new board.
+ 
+ 
+4 choices on what size board you want to sketch on:
+small board is 30x30
+medium board is 50x50
+large board is 80x80
+custom board will take any number (limitations between 25-100 not implemented this release);
+ 
+ 
+ 
+ 
+future release:
+clear board button that clears on click
+save images to a gallery that can be displayed below (stores last 5 images)
+custom input limitations (value, inputBox)
+ 
+Under the hood:
+When a button is clicked, a function will run creating columns and rows within the DOM. These dom elements are targeted by a mouseover event which triggers another function that turns individual cells into a different color.
--- script.js
+++ script.js
@@ -1,28 +1,42 @@
-
-
-var c = parseInt(prompt("Columns"));
-var r = parseInt(prompt("Rows"));
-var board = document.getElementById("customTable");
-
-
-for (var i = 0; i < r; i++) {
-    var newRow = document.createElement("tr");
-    newRow.classList.add("row");
-    for (var j = 0; j < c; j++) {
-        var newColumn = document.createElement("td");
-        newColumn.classList.add("column");
-        newRow.appendChild(newColumn);
-    }
-    board.appendChild(newRow);
-}
-
-function colorChange(e) {
-    e.target.style.backgroundColor = 'red';
+var board = document.getElementById("customTable"); //selects the table in html
+let small = document.getElementById("smallBoard");  //selects the small board button in html
+let medium = document.getElementById("mediumBoard"); //selects the medium board button in html
+let large = document.getElementById("largeBoard"); //slects the lardge board button in html
+ 
+function createPad(c) { //creates the board
+   board.innerHTML = " "; //clears the board
+   for (var i = 0; i < c; i++) { //creates the rows up to the value of c
+       var newRow = document.createElement("tr"); //create variable newRow, equals tr
+       newRow.classList.add("row"); //adds a class to the row
+   for (var j = 0; j < c; j++) { //creates the columns up to the value of c
+       var newColumn = document.createElement("td"); //creates a newColumn, equals td
+       newColumn.classList.add("column"); //adds a class to the column
+       newRow.appendChild(newColumn); //appends the column to the row
+   }
+   board.appendChild(newRow); //appends the row to the board
+   }
 };
-
-let colorCells = document.querySelectorAll(".row");
-
-colorCells.forEach(function(cell) { 
-cell.addEventListener("mouseover", colorChange); 
-});
+ 
+function game(c) { //creates the board that allows  mouseover
+   createPad(c); //calls the function to create the board
+   let colorCells = document.querySelectorAll(".column"); //selects all  of class .column (from newColumn)
+   function colorChange() { //changes the color of the cells
+       this.style.backgroundColor = 'red'; //changes the color of the cell that is currently mouseover to red
+       console.log(this); //logs the cell that is currently mouseover
+   };
+   colorCells.forEach(function(cell) {  //loops through the cells
+       cell.addEventListener("mouseover", colorChange);  //adds an event listener to cells when mousehover over them
+   });
+};
+ 
+function customButton() { //creates the custom board
+customInput.innerHtML = " "; //clears the inputBox
+let c = document.getElementById("userInput").value; //gets the value from the inputBox 
+game(c); //calls the function to create the board
+};
+ 
+small.addEventListener("click", () => game(30));    //calls the function to create the small board on click
+medium.addEventListener("click", () => game(50)); //calls the function to create the medium board on click
+large.addEventListener("click", () => game(70)); //calls the function to create the large board on click
+customBoard.addEventListener("click", customButton); //calls the function to create the custom board on click
 
